e9af62d1be75603e1be4d0d246c4ff8ba4ae89ec
OtherSomeone <maniacp3@gmail.com>
2022-11-19 16:59:55 -0300

    Iniciamos nuestra app

---
+++ run.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def inicio():
+    return ('Hola esta es la página de inicio')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
